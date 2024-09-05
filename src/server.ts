fff62acdcc69345b007f7b753753cf321fc41b3f
Dhushyanth H M <dxh8160@mavs.uta.edu>
2024-09-05 17:11:19 -0500

    removed explicit error

--- src/server.ts
+++ src/server.ts
@@ -25,13 +25,8 @@ app.post('/user', createNewUser)
 app.post('/signin', singin)
 
 app.use((err, req, res, next) => {
-    if(err.type === "auth"){
-        res.status(401).json({message: 'unauthorized'})
-    } else if(err.type === "input"){
-        res.status(400).json({message: 'invalid input'})
-    } else {
-        res.status(500).json({message: 'that is on us'})
-    }
-})
+    console.log(err)
+    res.json({message: `had an error: ${err.message}`})
+  })
 
 export default app
